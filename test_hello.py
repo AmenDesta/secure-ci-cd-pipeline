9e19b1808746b67de9aeb9553fc45144daeb3ce6
AmenDesta <amenwdesta@outlook.com>
2025-11-22 16:50:38 -0500

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,18 +1,13 @@
 from hello import greet, add, divide
 import pytest
 
-
 def test_greet():
-    assert greet() == "Hello, Secure World!"
+    assert greet("World") == "Hello, World!"
     assert greet("Amen") == "Hello, Amen!"
-
 
 def test_add():
     assert add(2, 3) == 5
 
-
 def test_divide():
-    assert divide(10, 2) == 5
-    with pytest.raises(ValueError):
+    with pytest.raises(ZeroDivisionError):
         divide(10, 0)
-
