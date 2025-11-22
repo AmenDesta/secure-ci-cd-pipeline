cecaefff6a1916dae16be1a62cc3f104431800cb
AmenDesta <amenwdesta@outlook.com>
2025-11-22 17:02:22 -0500

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -11,9 +11,13 @@ def test_greet():
 
 def test_add():
     assert add(2, 3) == 5
+    assert add(-1, 1) == 0
 
 
 def test_divide():
     with pytest.raises(ZeroDivisionError):
         divide(10, 0)
+
+
+
 
