455c94ea111adba8c1793085e113a1d020308ce0
AmenDesta <amenwdesta@outlook.com>
2025-11-22 17:19:10 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,15 +2,10 @@ def greet(name="Secure World!"):
     message = f"Hello, {name}"
     print(message)
     return message
-
 def add(a, b):
     return a + b
-
 def divide(a, b):
     return a / b
-
 # Simulated secret for Gitleaks to catch
 # SECRET_ACCESS = "12345"
-
 # Extra blank line below to trigger Flake8 rule
-
